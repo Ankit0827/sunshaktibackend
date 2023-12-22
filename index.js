78480b2d024222b972de1e60b6bff4ab9784fc6e
Ankit Verma <a.verma0827@gmail.com>
2023-12-22 17:17:17 +0530

    cors error resolved

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const app = express();
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: 'https://sunshaktisolar.com/',
     methods: 'POST',
     optionsSuccessStatus: 204,
 };
