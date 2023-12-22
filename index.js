fa272d415ac41999afef4d3411b87c16b6de9c21
Ankit Verma <a.verma0827@gmail.com>
2023-12-22 16:11:39 +0530

    form data

---
+++ index.js
@@ -0,0 +1,54 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); 
+
+app.post('/send-email', async (req, res) => {
+    const { company_name, phone, email, pincode, messages } = req.body;
+
+    // Create a nodemailer transporter
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASSWORD
+        }
+    });
+
+    // Email content
+    const mailOptions = {
+        from: email,
+        to: "a.verma0827@gmail.com",
+        subject: "Request for pricing",
+        text: `
+            Company Name: ${company_name}
+            Phone: ${phone}
+            Email: ${email}
+            Pincode: ${pincode}
+            Message: ${messages}
+        `
+    };
+
+    try {
+        const info = await transporter.sendMail(mailOptions); 
+        console.log("Email sent:", info.response);
+        res.status(200).send("Email sent successfully");
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Something went wrong");
+    }
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
