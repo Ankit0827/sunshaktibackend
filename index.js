5016e0dacdae6b257a13338ff48dfc6f4752da25
Ankit Verma <a.verma0827@gmail.com>
2024-01-10 16:43:19 +0530

    ...

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const app = express();
 
 const corsOptions = {
-    origin: 'https://sunshaktisolar.com',
+    origin: 'https://sunshaktisolar.com/',
     methods: 'POST',
     optionsSuccessStatus: 204,
 };
